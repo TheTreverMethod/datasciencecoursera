c496b248ed66f4ca8121f13cdade0b42d5009671
TheTreverMethod <sanborn.trev@gmail.com>
2017-12-19 11:38:07 -0600

    wip for best function

--- R Programming/Week 4/rprog-data-ProgAssignment3/best.R
+++ R Programming/Week 4/rprog-data-ProgAssignment3/best.R
@@ -1,17 +1,25 @@
 best <- function(state, outcome) {
     
     # Read outcome data
-    data <- getOutcomeData("outcome-of-care-measures.csv")
+    data <- get_outcomeData("outcome-of-care-measures.csv")
     
     # Validate state and outcome
-    validate_outcome()
-    validate_state()
+    validate_outcome(outcome)
+    validate_state(unique(data[,'State']), state)
     
     # Return hospital name with lowest rate
+    outcomeColumn <- get_outcomeColumnIndex(outcome)
+    indicesByStateAndOutcome <- get_validIndicesByStateAndOutcome(data, state, outcomeColumn)
+    min_outcomeValue <- min(data[indicesByStateAndOutcome,outcomeColumn])
+    best <- which(data[,'State'] == state & data[,outcomeColumn] == min_outcomeValue)
+    data[best,'Hospital.Name']
 }
 
 get_outcomeData <- function(fileName) {
     read.csv(fileName, colClasses = "character")
+}
+get_validIndicesByStateAndOutcome <- function(all_data, state, outcomeIndex) {
+    which(data[,'State'] == state & (!is.na(as.numeric(data[,outcomeIndex]))))
 }
 get_outcomeColumnIndex <- function(outcome) {
     if (outcome == "heart attack") {
